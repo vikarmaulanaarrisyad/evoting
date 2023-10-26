745fb3314dc5e053cc64f9b74e5fe646fd9e8a35
Vikar Maulana Arrisyad <vikar.maulana.arrisyad@gmail.com>
2023-10-26 19:30:51 +0700

    fig bug upload file excel

--- app/Imports/SiswaImport.php
+++ app/Imports/SiswaImport.php
@@ -4,9 +4,9 @@ namespace App\Imports;
 
 use App\Models\Siswa;
 use App\Models\User;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\ToCollection;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
 
@@ -14,29 +14,34 @@ class SiswaImport implements ToCollection, WithHeadingRow
 {
     public function collection(Collection $rows)
     {
-
         foreach ($rows as $row) {
-            $tanggal_lahir = $row['tanggal_lahir'];
-            $tanggal_lahir_valid = date("Y-m-d", strtotime($tanggal_lahir));
+            $nisn_siswa = $row['nisn_siswa'];
 
-            $user =  User::create([
-                'name'     => $row['nama'],
-                'username'     => $row['nisn_siswa'],
-                'email'    => $row['email'],
-                'password' => Hash::make($row['nisn_siswa']),
-                'role_id' => 3,
-            ]);
+            // Transaksi Database
+            DB::transaction(function () use ($row, $nisn_siswa) {
+                $user = User::updateOrCreate(
+                    ['username' => $nisn_siswa],
+                    [
+                        'name'     => $row['nama'],
+                        'email'    => $row['email'],
+                        'password' => Hash::make($nisn_siswa),
+                        'role_id'  => 3,
+                    ]
+                );
 
-            $siswa =  Siswa::create([
-                'user_id'     => $user->id,
-                'nama_siswa'    => $row['nama'],
-                'nisn_siswa'    => $row['nisn_siswa'],
-                'nis_siswa' => $row['nis_siswa'],
-                'tingkat_siswa' => $row['tingkat'],
-                'tempat_lahir_siswa' => $row['tempat_lahir'],
-                'tanggal_lahir_siswa' => $tanggal_lahir_valid,
-                'status_pemilihan_siswa' => 'Belum Memilih',
-            ]);
+                Siswa::updateOrCreate(
+                    ['nisn_siswa' => $nisn_siswa],
+                    [
+                        'user_id'                => $user->id,
+                        'nama_siswa'             => $row['nama'],
+                        'nis_siswa'              => $row['nis_siswa'],
+                        'tingkat_siswa'          => $row['tingkat'],
+                        'tempat_lahir_siswa'     => $row['tempat_lahir'],
+                        'tanggal_lahir_siswa'    => date("Y-m-d", strtotime($row['tanggal_lahir'])),
+                        'status_pemilihan_siswa' => 'Belum Memilih',
+                    ]
+                );
+            });
         }
     }
 }
--- resources/views/admin/siswa/import.blade.php
+++ resources/views/admin/siswa/import.blade.php
@@ -1,4 +1,4 @@
-<x-modal id="import" data-backdrop="static" data-keyboard="false" size="modal-md">
+<x-modal id="import" data-backdrop="static" data-keyboard="false" size="modal-lg">
     <x-slot name="title">
         Import Data
     </x-slot>
@@ -7,12 +7,12 @@
     <div class="row">
         <div class="col-md-12">
             <div class="form-group">
-                <label for="">Upload file</label>
                 <input type="file" class="custom-file-input" id="customFile" name="upload">
                 <label class="custom-file-label" for="customFile">Choose file</label>
             </div>
         </div>
     </div>
+
     <x-slot name="footer">
         <button type="button" onclick="submitForm(this.form)" class="btn btn-sm btn-outline-primary" id="submitBtn">
             <span id="spinner-border" class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
